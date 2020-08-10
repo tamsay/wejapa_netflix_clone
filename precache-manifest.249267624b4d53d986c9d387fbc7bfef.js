370897b91d72576d6a5da8ac263969b5ad1e5932
tamsay <longetemitope@gmail.com>
2020-08-10 02:18:55 +0100

    First build of the netflix clone - header and footer components

---
+++ precache-manifest.249267624b4d53d986c9d387fbc7bfef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b425e495b157bde51cc034e46c7c4132",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37dbb6af2c7ed67eed40",
+    "url": "/static/css/main.a1d89363.chunk.css"
+  },
+  {
+    "revision": "2da1b16b4d8bc2a69643",
+    "url": "/static/js/2.7ebaa6b0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7ebaa6b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37dbb6af2c7ed67eed40",
+    "url": "/static/js/main.e1d1cfbe.chunk.js"
+  },
+  {
+    "revision": "07b33697149324db54e5",
+    "url": "/static/js/runtime-main.b8a87127.js"
+  }
+]);
